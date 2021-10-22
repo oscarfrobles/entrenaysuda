935093c5bff25abeeeefa5f71a0c8f9330384423
Oscar <oskijob@gmail.com>
2021-10-22 20:56:22 +0200

    calendario de la home

---
+++ static/js/calendario.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    pickmeup.defaults.locales['es'] = {
+  	  days: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+  	  daysShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sáb'],
+  	  daysMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+  	  months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+  	  monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
+    };
+
+    pickmeup('.fecha', {
+		flat : true,
+			date      : [
+  			new Date
+  		],
+  		mode      : 'single',
+	});
+
+    console.log('hola');
+    console.log(cal);
+
+});
