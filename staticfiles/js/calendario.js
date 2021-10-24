9e13642d144dab42bc7d5b825ab597b2b256ab0a
Oscar <oskijob@gmail.com>
2021-10-24 23:30:57 +0200

    main ci

--- staticfiles/js/calendario.js
+++ staticfiles/js/calendario.js
@@ -1,6 +1,6 @@
 jQuery(function ($){
 
-deb = 0;
+deb = 1;
 
 debug = function(param){
     if (deb==1){
@@ -23,10 +23,10 @@ $(document).ready(function(){
             comp = "<span class='rojo'>NO REALIZADO</span>";
             break;
             case 1:
-            comp = "<span class='naranja'>COMPLETO</span>";
+            comp = "<span class='verde'>COMPLETO</span>";
             break;
             case 2:
-            comp = "<span class='verde'>SEMICOMPLETO</span>";
+            comp = "<span class='naranja'>SEMICOMPLETO</span>";
             break;
         }
         return comp;
@@ -43,6 +43,14 @@ $(document).ready(function(){
             data[fecha]['comentario'] = json_cal[i]['comentario'];
             data[fecha]['completado'] = json_cal[i]['completado'];
             data[fecha]['series'] = json_cal[i]['series'];
+            if(json_cal[i]['calories'])
+                 data[fecha]['calories'] = json_cal[i]['calories'];
+            if(json_cal[i]['bpm'])
+                 data[fecha]['bpm'] = json_cal[i]['bpm'];
+            if(json_cal[i]['distance'])
+                 data[fecha]['distance'] = json_cal[i]['distance'];
+            if(json_cal[i]['weight'])
+                 data[fecha]['weight'] = json_cal[i]['weight'];
 
             data[fecha]['ejercicios'].push(json_cal[i]['ejercicios__nombre']);
             debug(data[fecha]['ejercicios']);
@@ -53,6 +61,7 @@ $(document).ready(function(){
             if(json_cal[i]['completado'] == 2)
                 data[fecha]['class_bg'] = 'naranja';
         }
+        debug('aqui');
     }
 
         init();
@@ -88,21 +97,23 @@ $(document).ready(function(){
 
 
 	$(".fecha").on('pickmeup-change', function (e) {
-	   // dt = new Date();
-        //debug(e.detail.formatted_date); // New date according to current format
-        ej = null;
+//	   $('.calorias-txt, .bpm-txt, .distance-txt, .weight-txt', window.parent.document).attr("class", "hide");
+       $('.calorias-txt, .bpm-txt, .distancia-txt, .peso-txt', window.parent.document).removeClass("show").addClass("hide");
+       $('.calorias-txt > span,  .bpm-txt > span, .distancia-txt > span, .peso-txt > span').empty();
+
         cal_data = data[e.detail.formatted_date];
-        if(cal_data == undefined){
+        debug(cal_data);
+        if(cal_data == undefined || Array.isArray(cal_data)== false){
             return true;
         }
 
         jQuery.noConflict();
         $("#modalcalendario", window.parent.document).modal();
-        if (Array.isArray(cal_data) && cal_data['ejercicios'] != undefined){
-            ej = cal_data['ejercicios'];
-        }
-        if (null != ej && ej.length > 0){
-            jQuery.each(ej, function(index, value) {
+
+        if (Array.isArray(cal_data['ejercicios']) && cal_data['ejercicios'].length > 0){
+            $('.ejercicios-txt > li', window.parent.document).remove();
+            jQuery.each(cal_data['ejercicios'], function(index, value) {
+                debug(index + " " + value);
                  $('<li>').text(value).appendTo($('.ejercicios-txt', window.parent.document));
             });
             //$('.ejercicios-txt', window.parent.document).html(ej.join('<br>'));
@@ -111,6 +122,28 @@ $(document).ready(function(){
         $('.completado-txt', window.parent.document).html(completado(cal_data['completado']));
         $('.comentario-txt', window.parent.document).html(cal_data['comentario']);
         $('.series-txt', window.parent.document).html(cal_data['series']);
+
+        if(undefined != cal_data['calories'] && $.isNumeric(cal_data['calories'])){
+            debug("calories: "+ cal_data['calories']);
+            $('.calorias-txt > span', window.parent.document).html(cal_data['calories']);
+            $('.calorias-txt', window.parent.document).addClass("show").removeClass("hide");
+
+        }
+        if(undefined != cal_data['bpm'] && $.isNumeric(cal_data['bpm'])){
+            debug("bpm: "+ cal_data['bpm']);
+            $('.bpm-txt > span', window.parent.document).html(cal_data['bpm']);
+            $('.bpm-txt', window.parent.document).addClass("show").removeClass("hide");
+        }
+        if(undefined != cal_data['distance'] && $.isNumeric(cal_data['distance'])){
+        debug("distance: "+ cal_data['distance']);
+            $('.distancia-txt > span', window.parent.document).html(cal_data['distance']);
+            $('.distancia-txt', window.parent.document).addClass("show").removeClass("hide");;
+        }
+        if(undefined != cal_data['weight'] && $.isNumeric(cal_data['weight'])){
+        debug("weight: "+ cal_data['weight']);
+            $('.peso-txt > span', window.parent.document).html(cal_data['weight']);
+            $('.peso-txt', window.parent.document).addClass("show").removeClass("hide");
+        }
      });
 
 
